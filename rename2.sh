f9d84bbe24857ed90ebe9291cdd2e7d630a213d8
Matt Postiff <postiffm@umich.edu>
2023-12-04 19:26:17 -0500

    Rename files to match normal standard

---
+++ rename2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Put file names back to what they were before
+git mv 40_Matthew.usfm           41MATCDFBIV.SFM 
+git mv 41_Mark.usfm		 42MRKCDFBIV.SFM 
+git mv 42_Luke.usfm		 43LUKCDFBIV.SFM 
+git mv 43_John.usfm		 44JHNCDFBIV.SFM 
+git mv 44_Acts.usfm		 45ACTCDFBIV.SFM 
+git mv 45_Romans.usfm		 46ROMCDFBIV.SFM 
+git mv 46_1_Corinthians.usfm	 471COCDFBIV.SFM 
+git mv 47_2_Corinthians.usfm	 482COCDFBIV.SFM 
+git mv 48_Galatians.usfm	 49GALCDFBIV.SFM 
+git mv 49_Ephesians.usfm	 50EPHCDFBIV.SFM 
+git mv 50_Philippians.usfm	 51PHPCDFBIV.SFM 
+git mv 51_Colossians.usfm	 52COLCDFBIV.SFM 
+git mv 52_1_Thessalonians.usfm	 531THCDFBIV.SFM 
+git mv 53_2_Thessalonians.usfm	 542THCDFBIV.SFM 
+git mv 54_1_Timothy.usfm	 551TICDFBIV.SFM 
+git mv 55_2_Timothy.usfm	 562TICDFBIV.SFM 
+git mv 56_Titus.usfm		 57TITCDFBIV.SFM 
+git mv 57_Philemon.usfm		 58PHMCDFBIV.SFM 
+git mv 58_Hebrews.usfm		 59HEBCDFBIV.SFM 
+git mv 59_James.usfm		 60JASCDFBIV.SFM 
+git mv 60_1_Peter.usfm		 611PECDFBIV.SFM 
+git mv 61_2_Peter.usfm		 622PECDFBIV.SFM 
+git mv 62_1_John.usfm		 631JNCDFBIV.SFM 
+git mv 63_2_John.usfm		 642JNCDFBIV.SFM 
+git mv 64_3_John.usfm		 653JNCDFBIV.SFM 
+git mv 65_Jude.usfm		 66JUDCDFBIV.SFM 
+git mv 66_Revelation.usfm	 67REVCDFBIV.SFM
+
